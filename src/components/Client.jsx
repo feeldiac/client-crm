76ec01acc9e65eb3a8076d77e36e47130819cf2b
Fernando Díaz <feeldiac@gmail.com>
2022-09-11 11:59:12 -0500

    Created the client row for list all clients in json server

---
+++ src/components/Client.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Client = ({ client }) => {
+
+    const { name, company, email, telephone, notes, id } = client
+
+    return (
+        <tr className='border-b hover:bg-gray-50'>
+            <td className="p-3">{name}</td>
+            <td className="p-3">
+                <p><span className='text-gray-800 font-bold'>Email: </span>{email}</p>
+                <p><span className='text-gray-800 font-bold'>Tel: </span>{telephone}</p>
+            </td>
+            <td className="p-3">{company}</td>
+            <td className="p-3">
+                <button
+                    className='bg-yellow-500 hover:bg-yellow-600 block w-full text-white p-2 
+                    font-bold text-xs'
+                    type='button'>
+                    Inspect
+                </button>
+                <button
+                    className='bg-blue-600 hover:bg-blue-700 block w-full text-white p-2 
+                    font-bold text-xs mt-3'
+                    type='button'>
+                    Edit
+                </button>
+                <button
+                    className='bg-red-600 hover:bg-red-700 block w-full text-white p-2 
+                    font-bold text-xs mt-3'
+                    type='button'>
+                    Delete
+                </button>
+            </td>
+        </tr>
+    )
+}
+
+export default Client
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import Client from '../components/Client'
 
 const Home = () => {
+
+    const [clients, setClients] = useState([])
+
+    useEffect(() => {
+        const getClientsAPI = async () => {
+            try {
+                const url = 'http://localhost:4000/clients'
+                const response = await fetch(url)
+                const result = await response.json()
+                setClients(result)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        getClientsAPI()
+    }, [])
+
     return (
-        <div>From Home</div>
+        <>
+            <h1 className='font-black text-4xl text-blue-800'>Clients</h1>
+            <p>Manage your clients</p>
+
+            <table className='w-full mt-5 table-auto shadow bg-white'>
+                <thead className='bg-blue-800 text-white'>
+                    <tr>
+                        <th className='p-2'>Name</th>
+                        <th className='p-2'>Contact</th>
+                        <th className='p-2'>Company</th>
+                        <th className='p-2'>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {clients.map(client => (
+                        <Client
+                            key={client.id}
+                            client={client}
+                        />
+                    ))}
+                </tbody>
+            </table>
+        </>
     )
 }
 
